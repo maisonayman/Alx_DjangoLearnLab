4091f0bde7b0d2b0e0f4b1e98a78313e91ae2442
maisonayman <maisonsayed2002@gmail.com>
2025-09-09 02:58:57 +0300

    new task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,31 +4,15 @@ from django.http import HttpResponse
 from .models import Book, Library
 from django.views.generic import DetailView
 
-# Create your views here.
 
-@api_view(['GET'])
-
+# Function-Based View
 def list_books(request):
     books = Book.objects.all()
-    
-    if not books:
-        return HttpResponse("No books found.")
-    
-    response_text = "Books in the database:\n\n"
-    for book in books:
-        response_text += f"- {book.title} (Author: {book.author.name})\n"
-    
-    return HttpResponse(response_text, content_type="text/plain")
+    return render(request, "list_books.html", {"books": books})
 
 
+# Class-Based View
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"   
-    context_object_name = "library" 
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["books"] = self.object.books.all()
-        return context
-
-
+    template_name = "library_detail.html"
+    context_object_name = "library"
