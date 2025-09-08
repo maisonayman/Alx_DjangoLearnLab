eef15e73f63299b308275fc5022029f7a695126c
maisonayman <maisonsayed2002@gmail.com>
2025-09-09 01:08:54 +0300

    new task

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,23 +2,27 @@ import django
 import os
 import sys
 
-sys.path.append(r"D:/ALX_tasks/Alx_DjangoLearnLab/django-models/LibraryProject")
+# خلي Python يعرف مكان المشروع
+sys.path.append(r"D:\ALX_tasks\Alx_DjangoLearnLab\django-models\LibraryProject")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
-
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
+# Variables for queries
+author_name = "George Orwell"
+library_name = "Central Library"
+
 # 1. Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
-print("Books by George Orwell:")
+print(f"Books by {author_name}:")
 for book in books_by_author:
     print("-", book.title)
 
 # 2. List all books in a library
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name=library_name)  # ✅ ده اللي الرسالة طالبه
 print(f"\nBooks in {library.name}:")
 for book in library.books.all():
     print("-", book.title)
