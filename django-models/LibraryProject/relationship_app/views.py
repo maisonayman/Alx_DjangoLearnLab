df7c4ea2b3c34e1165c984df65c8b67cad6aa81a
maisonayman <maisonsayed2002@gmail.com>
2025-09-09 03:01:48 +0300

    new task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from django.http import HttpResponse
 from .models import Book, Library
 from django.views.generic import DetailView
 
@@ -8,11 +6,11 @@ from django.views.generic import DetailView
 # Function-Based View
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 # Class-Based View
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
