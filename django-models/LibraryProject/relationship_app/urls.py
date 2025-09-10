b9e4af819dfcc2975fdc8dd471d10af6717c5ea6
maisonayman <maisonsayed2002@gmail.com>
2025-09-10 03:39:45 +0300

    new task

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import LibraryDetailView
-from .views import list_books
+from .views import list_books, register
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    path("register/", register, name="register"),  # function-based view
+    path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView   
-
+from .forms import RegisterForm
 
 # Function-Based View
 def list_books(request):
@@ -15,3 +15,17 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')   # بعد التسجيل روح للصفحة login
+    else:
+        form = RegisterForm()
+    return render(request, 'relationship_app/register.html', {"form": form})
