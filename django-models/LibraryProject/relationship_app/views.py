92ea2876371b34dd3192a8d47d2d2e0a7742f5b3
maisonayman <maisonsayed2002@gmail.com>
2025-09-13 22:56:49 +0300

    new task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import  permission_required ,user_passes_test
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import Book, Library
 
 
