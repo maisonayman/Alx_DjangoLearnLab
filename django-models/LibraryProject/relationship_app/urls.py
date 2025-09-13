eaf18b5777e8af4ccbd385d44de885c2019ffdbe
maisonayman <maisonsayed2002@gmail.com>
2025-09-13 22:58:26 +0300

    new task

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,9 +8,9 @@ from .librarian_view import librarian_dashboard
 from .member_view import member_dashboard
 
 urlpatterns = [
-    path("books/add/", add_book, name="add_book"),
-    path("books/<int:book_id>/edit/",edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", delete_book, name="delete_book"),
+    path("add/books/", add_book, name="add_book"),
+    path("edit/books/<int:book_id>/",edit_book, name="edit_book"),
+    path("books/delete/<int:book_id>/", delete_book, name="delete_book"),
     path("books/", list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
     path("register/", register, name="register"),  # function-based view
