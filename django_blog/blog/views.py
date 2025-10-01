92b3464dfbc7a866e4d2d13b3a16b0b61a7743e9
maisonayman <maisonsayed2002@gmail.com>
2025-10-01 23:50:52 +0300

    new task

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+        
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form": form})    
+    
+
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html")
+
+
+class CustomLoginView(LoginView):
+    template_name = "blog/login.html"
+
+
+class CustomLogoutView(LogoutView):
+    template_name = "blog/logout.html"
