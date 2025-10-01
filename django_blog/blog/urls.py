b5aa61ab055d617233349a05c254a91e3f6d404b
maisonayman <maisonsayed2002@gmail.com>
2025-10-01 23:51:28 +0300

    new task

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import register, profile, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
-    path("register", register, name="register"),
-    path("login", CustomLoginView.as_view(), name="login"),
-    path("logout", CustomLogoutView.as_view(), name="logout"),
-    path("profile", profile, name="profile"),
+    path("register/", register, name="register"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("profile/", profile, name="profile"),
 ]
