c32b6cd2947226a7ff0e0d3698a450a7d569caa8
maisonayman <maisonsayed2002@gmail.com>
2025-10-08 22:59:09 +0300

    new task

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('posts/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
 ]
