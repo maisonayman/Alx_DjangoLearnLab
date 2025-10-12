01258ddad1ea717e257f91b5752a9eff6a33e3bf
maisonayman <maisonsayed2002@gmail.com>
2025-10-13 00:54:23 +0300

    newtask

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,7 +5,10 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth import get_user_model
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from .models import User
 # Create your views here.
 
 User = get_user_model()
@@ -36,3 +39,28 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+    if request.user == target_user:
+        return Response({'error': "You cannot follow yourself."}, status=400)
+    
+    request.user.following.add(target_user)
+    return Response({'message': f'You are now following {target_user.username}.'})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+    if target_user in request.user.following.all():
+        request.user.following.remove(target_user)
+        return Response({'message': f'You unfollowed {target_user.username}.'})
+    return Response({'error': 'You are not following this user.'}, status=400)
+
+
+
--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, user_feed
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('feed/', user_feed, name='user-feed'),
+]
+
+urlpatterns += router.urls
