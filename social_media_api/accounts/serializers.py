521d440405dbe98199fb0202a100f8112c6242e4
maisonayman <maisonsayed2002@gmail.com>
2025-10-13 00:17:12 +0300

    newtask

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,7 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password']
