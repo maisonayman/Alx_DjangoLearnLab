b4520096ce733d04dcf40727a6c8a9c77a8518aa
maisonayman <maisonsayed2002@gmail.com>
2025-10-19 19:00:05 +0300

    host

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-default-key')  
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = False
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # APPS
